b2199e8696622fce3000d0c7263a2c809ff8edc2
ronaldhgamez <ronaldhgamez@gmail.com>
2021-10-19 13:17:38 -0600

    login and register added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LoginComponentComponent } from './login-component/login-component.component';
+import { RegisterComponentComponent } from './register-component/register-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponentComponent
+    LoginComponentComponent,
+    RegisterComponentComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/register-component/register-component.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-component',
+  templateUrl: './register-component.component.html',
+  styleUrls: ['./register-component.component.css']
+})
+export class RegisterComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  saveEmployee() {
+    const name = (<HTMLInputElement>document.getElementById("name")).value;
+    const card_id = (<HTMLInputElement>document.getElementById("card_id")).value;
+    const tel = (<HTMLInputElement>document.getElementById("tel")).value;
+    const emergency = (<HTMLInputElement>document.getElementById("emergency")).value;
+    const age = (<HTMLInputElement>document.getElementById("age")).value;
+
+    
+  }
+}
